0a1494e365bd1f120a57eef757510593a80ad2d0
senthil kumar <senthilkmaar@gmail.com>
2021-07-27 22:35:42 +0530

    company api & gateway api added

---
+++ src/Services/Company/Company.API/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Company.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Company.API.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
